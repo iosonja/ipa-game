dd610ba74511dd71ad51074f2bf4d031bdedd378
Sonja Ek <sonja3k@icloud.com>
2021-03-24 12:42:39 +0200

    Task 2.4: add remaining tests

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -1,9 +1,13 @@
 import unittest
 from kassapaate import Kassapaate
+from maksukortti import Maksukortti
 
 class TestKassapaate(unittest.TestCase):
     def setUp(self):
         self.kassa = Kassapaate()
+        self.kortti = Maksukortti(1000)
+
+    # Käteisostot:
 
     def test_luotu_kassa_on_olemassa(self):
         self.assertNotEqual(self.kassa, None)
@@ -51,3 +55,35 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.syo_maukkaasti_kateisella(50)
         maukkaat = self.kassa.maukkaat
         self.assertEqual(maukkaat, 0)
+
+    # Korttiostot:
+
+    def test_edullinen_lounas_veloitetaan_kortilta(self):
+        success = self.kassa.syo_edullisesti_kortilla(self.kortti)
+        self.assertEqual(success, True)
+
+    def test_maukas_lounas_veloitetaan_kortilta(self):
+        success = self.kassa.syo_maukkaasti_kortilla(self.kortti)
+        self.assertEqual(success, True)
+
+    def test_edullista_lounasta_ei_veloiteta_jos_saldo_ei_riita(self):
+        self.kassa.syo_maukkaasti_kortilla(self.kortti)
+        self.kassa.syo_maukkaasti_kortilla(self.kortti)
+        success = self.kassa.syo_edullisesti_kortilla(self.kortti)
+        self.assertEqual(success, False)
+
+    def test_maukasta_lounasta_ei_veloiteta_jos_saldo_ei_riita(self):
+        self.kassa.syo_maukkaasti_kortilla(self.kortti)
+        self.kassa.syo_maukkaasti_kortilla(self.kortti)
+        success = self.kassa.syo_maukkaasti_kortilla(self.kortti)
+        self.assertEqual(success, False)
+
+    def test_positiivisen_summan_lataus_nakyy_kassassa(self):
+        self.kassa.lataa_rahaa_kortille(self.kortti, 600)
+        rahaa = self.kassa.kassassa_rahaa
+        self.assertEqual(rahaa, 100600)
+
+    def test_negatiivisen_summan_lataus_ei_nay_kassassa(self):
+        self.kassa.lataa_rahaa_kortille(self.kortti, -600)
+        rahaa = self.kassa.kassassa_rahaa
+        self.assertEqual(rahaa, 100000)
