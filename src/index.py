a0c61aeb3b7bef874fb83f662d13bfbe46ccff16
Sonja Ek <sonja3k@icloud.com>
2021-04-19 16:02:09 +0300

    Formatting changes made with autopep8

--- src/index.py
+++ src/index.py
@@ -1,6 +1,7 @@
 import pygame
 from button import Button
 
+
 pygame.init()
 
 SCREEN_WIDTH = 1000
@@ -17,7 +18,7 @@ VELOCITY = 5
 
 
 # ----- Button creation -----
-button_width  = 60
+button_width = 60
 button_height = 25
 x_button = 10
 y_button = SCREEN_HEIGHT - button_height
@@ -28,7 +29,7 @@ button = Button(x_button, y_button, False)
 # ----- Scores section ------
 # when this reaches the number of alphabets in the game, the player WINDOWs.
 score = 0
-NBR_OF_ALPHABETS = 20 # Change later
+NBR_OF_ALPHABETS = 20  # Change later
 
 # ----- End scores section -----
 
@@ -52,16 +53,17 @@ while run:
 
     if x_bubble >= SCREEN_WIDTH + BUBBLE_RADIUS:
         # The bubble has reached the right end. Some punishment will be added here.
-        pygame.quit() # Remove this later
+        pygame.quit()  # Remove this later
 
     if score >= NBR_OF_ALPHABETS:
         # The player has won. Add a banner asking if the player wants to play again.
-        pygame.quit() # Remove this later
+        pygame.quit()  # Remove this later
 
     WINDOW.fill((212, 220, 255))
-    pygame.draw.circle(WINDOW, (144, 122, 214), (x_bubble, y_bubble), BUBBLE_RADIUS) # bubble
-    pygame.draw.rect(WINDOW, (79, 81, 140), (x_button, y_button, button_width, 
-    button_height), 3) # button
+    pygame.draw.circle(WINDOW, (144, 122, 214), (x_bubble,
+                       y_bubble), BUBBLE_RADIUS)  # bubble
+    pygame.draw.rect(WINDOW, (79, 81, 140), (x_button, y_button, button_width,
+                                             button_height), 3)  # button
     pygame.display.update()
 
 pygame.quit()
--- src/tests/button_test.py
+++ src/tests/button_test.py
@@ -1,10 +1,10 @@
 import unittest
 from button import Button
 
+
 class TestButton(unittest.TestCase):
     def setUp(self):
         self.button = Button(10, 100, False)
 
     def test_button_exists_after_creation(self):
         self.assertNotEqual(self.button, None)
-
