1a677cff5f27f9001f7071ea20508cfdc3c9a62a
Sonja Ek <sonja3k@icloud.com>
2021-04-20 22:39:41 +0300

    Add scores tracker

---
+++ src/scores.py
@@ -0,0 +1,9 @@
+class Scores:
+    def __init__(self):
+        self.number = 0
+
+    def increase(self, amount):
+        self.number += amount
+
+    def make_zero(self):
+        self.number = 0
