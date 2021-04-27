3c2f849a61c198761c99fff32b96aad537c15ef1
Sonja Ek <sonja3k@icloud.com>
2021-04-27 17:43:35 +0300

    Update instructions window text

--- src/ui/renderer.py
+++ src/ui/renderer.py
@@ -2,30 +2,32 @@ import pygame
 
 
 class Textbox:
-    def __init__(self, window):
+    def __init__(self, window, width):
         self.font = pygame.font.SysFont('Arial', 20)
         self._window = window
+        self._width = width
         self.rect = pygame.draw.rect(
-            self._window, (0, 0, 0), (0, 410, 1200, 100), 1)
+            self._window, (0, 0, 0), (0, 410, self._width, 100), 1)
 
     def add_text(self):
-        text1 = """Welcome! Try to make the bubble stop before it reaches the right edge. In the current version of the game, the stopping can be"""
-        text2 = """done by pressing R on your keyboard if the bubble is red, G if green and B if blue. You can close the game by pressing X."""
-        # text3 = ""
+        text1 = """Welcome! Try to make the bubble stop before it reaches the right edge. In the current version of the game, the stopping"""
+        text2 = """can be done by pressing R on your keyboard if the bubble is red, G if green, B if blue, W if white and P if pink."""
+        text3 = "You can close the game by pressing X."
         self._window.blit(self.font.render(text1, True, (0, 0, 0)), (10, 420))
         self._window.blit(self.font.render(text2, True, (0, 0, 0)), (10, 440))
-        # self._window.blit(self.font.render(text3, True, (0, 0, 0)), (10, 50))
+        self._window.blit(self.font.render(text3, True, (0, 0, 0)), (10, 460))
 
 
 class Renderer:
-    def __init__(self, window, background_color):
+    def __init__(self, window, width, background_color):
         self._window = window
+        self._width = width
         self._background_color = background_color
 
     def redraw(self, bubble_color, x_bubble, y_bubble, bubble_radius):
         self._window.fill(self._background_color)
         pygame.draw.circle(self._window, bubble_color, (x_bubble,
                                                         y_bubble), bubble_radius)
-        textbox = Textbox(self._window)
+        textbox = Textbox(self._window, self._width)
         textbox.add_text()
         pygame.display.update()
