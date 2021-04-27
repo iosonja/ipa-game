9c9b1b651c77c5c25ae10329e231b198f856584e
Sonja Ek <sonja3k@icloud.com>
2021-04-27 16:35:45 +0300

    Add event queue

--- src/index.py
+++ src/index.py
@@ -1,20 +1,22 @@
 import pygame
 from ui.renderer import Renderer
-from ui.gameloop import GameLoop
+from gameloop import GameLoop
+from event_queue import EventQueue
 
-SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 500
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 500
 BACKGROUND_COLOR = (212, 220, 255)
 
 
 def main():
-    pygame.init()
-    window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption("IPA game")
 
     renderer = Renderer(window, BACKGROUND_COLOR)
-    loop = GameLoop(window, SCREEN_WIDTH, renderer)
+    event_queue = EventQueue()
+    loop = GameLoop(window, WINDOW_WIDTH, renderer, event_queue)
 
+    pygame.init()
     loop.start()
 
 
