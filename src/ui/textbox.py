118af9250f2175011a95ff6855adf1ad6bece95e
Sonja Ek <sonja3k@icloud.com>
2021-05-03 17:27:32 +0300

    Tweak text box graphics

--- src/ui/textbox.py
+++ src/ui/textbox.py
@@ -3,22 +3,20 @@ import pygame
 
 class Textbox:
     def __init__(self, window, width, score_tracker):
-        self.font = pygame.font.SysFont('Arial', 20)
+        self.score_font = pygame.font.SysFont('ptmono', 25)
+        self.info_font = pygame.font.SysFont('ptmono', 20)
         self._window = window
         self._width = width
         self._score_tracker = score_tracker
-        self.rect = pygame.draw.rect(
-            self._window, (0, 0, 0), (0, 410, self._width, 100), 1)
+        pygame.draw.line(self._window, (0, 0, 0),
+                         (0, 450), (self._width, 450), 1)
 
     def add_text(self):
-        text1 = """Welcome! Try to make the bubble stop before it reaches the right edge. In the current version of the game, the stopping"""
-        text2 = """can be done by pressing R on your keyboard if the bubble is red, G if green, B if blue, W if white and P if pink."""
-        text3 = "You can close the game by pressing X."
+        text1 = "Try to make the bubble stop before it reaches the right edge. You can close the game by pressing X."
         score_text = "Current score: {}".format(
             self._score_tracker.current_score)
 
-        self._window.blit(self.font.render(text1, True, (0, 0, 0)), (10, 420))
-        self._window.blit(self.font.render(text2, True, (0, 0, 0)), (10, 440))
-        self._window.blit(self.font.render(text3, True, (0, 0, 0)), (10, 460))
-        self._window.blit(self.font.render(
+        self._window.blit(self.score_font.render(
             score_text, True, (0, 0, 0)), (10, 10))
+        self._window.blit(self.info_font.render(
+            text1, True, (0, 0, 0)), (10, 460))
